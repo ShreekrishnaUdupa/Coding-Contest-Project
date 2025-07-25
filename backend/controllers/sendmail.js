f8b078678778dbdf91f10d3fd0b896d5fc691b9d
shreekrishnaudupa <shreekrishnaudupa@gmail.com>
2025-07-25 15:35:46 +0530

    Added the connection to Neon database

---
+++ backend/controllers/sendmail.js
@@ -0,0 +1,26 @@
+const nodemailer = require ('nodemailer');
+
+const sendMail = async () => {
+
+    const transporter = nodemailer.createTransport (
+    {
+        secure: true,
+        host: 'smtp.gmail.com',
+        port: 465,
+        auth: {
+            user: 'shreekrishnaudupa@gmail.com',
+            pass: 'svhqrkyfbvoxbuma'
+        }
+    });
+
+    await transporter.sendMail ({
+        from: 'Shreekrishna shreekrishnaudupa@gmail.com',
+        to: 'udupashreekrishna@gmail.com',
+        subject: 'Mail from Shreekrishna',
+        html: 'Hello, if you are seeing this, it means it worked'
+    });
+
+    console.log('Email sent');
+};
+
+sendMail ();
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,27 @@
 const express = require ('express');
 const cors = require ('cors');
+const { Client } = require ('pg');
+
+require('dotenv').config();
 
 const app = express ();
 
 app.use(cors({origin: '*'}));
 app.use(express.json());
+
+const client = new Client ({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false}
+});
+
+client.connect ()
+    .then(() => {
+        console.log('Connected to the database successfully');
+
+        app.listen(3000, () => 
+            console.log('Server running on port 4000'));
+    })
+
+    .catch ((error) =>
+        console.log('Connection to database failed')
+);
